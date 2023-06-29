48caaac43c0f4ef4eb787be13225c7bec91edd77
Daniel <d.holguin@protonmail.com>
2023-06-28 20:04:25 -0700

    before chaning circle

--- src/main.rs
+++ src/main.rs
@@ -40,12 +40,20 @@ fn model(app: &App) -> Model {
         stream,
         volume,
         fft_output,
-        previous_hue: Arc::new(Mutex::new(0.6)),
-        previous_frequency_multiplier: Arc::new(Mutex::new(0.0)),
-        previous_circle_radius: Arc::new(Mutex::new(0.0)),
+        previous_hue: 0.6,
+        previous_frequency_multiplier: 0.0,
+        previous_circle_radius: 50.0,
         hue: 0.0,
         high_freq_sum: 0.0,
         low_freq_sum: 0.0,
+        string_points: Vec::new(),
+        circle_radius: 0.0,
+        line_color: nannou::color::hsl(0.0, 0.0, 0.0).into(), // Setting initial color to black
+        circle_color: nannou::color::hsl(0.0, 0.0, 0.0).into(),
+        prev_power_spectrum: Vec::new(),
+        past_magnitudes: vec![vec![0.0; 10]; 6],
+        past_spectral_flux: Vec::new(),
+        cooldown_counter: 0,
     }
 }
 
@@ -53,8 +61,10 @@ fn key_pressed(app: &App, model: &mut Model, key: Key) {
     match key {
         Key::Space => {
             let assets = app.assets_path().expect("could not find assets directory");
-            let path = assets.join("driver.wav");
+            let path = assets.join("time.wav");
             let sound = audrey::open(path).expect("failed to load sound");
+
+            // Update the sample rate in the audio model
             model
                 .stream
                 .send(move |audio| {
@@ -69,131 +79,154 @@ struct Model {
     stream: audio::Stream<Audio>,
     volume: Arc<Mutex<f32>>,
     fft_output: Arc<Mutex<Vec<Complex<f32>>>>,
-    previous_hue: Arc<Mutex<f32>>,
-    previous_frequency_multiplier: Arc<Mutex<f32>>,
-    previous_circle_radius: Arc<Mutex<f32>>,
+    previous_hue: f32,
+    previous_frequency_multiplier: f32,
+    previous_circle_radius: f32,
     hue: f32,
     high_freq_sum: f32,
     low_freq_sum: f32,
+    string_points: Vec<Vec<Point2>>,
+    circle_radius: f32,
+    line_color: LinSrgba,
+    circle_color: LinSrgba,
+    prev_power_spectrum: Vec<f32>,
+    past_magnitudes: Vec<Vec<f32>>,
+    past_spectral_flux: Vec<f32>,
+    cooldown_counter: usize,
 }
+fn process_fft_output(fft_output: &[f32], prev_power_spectrum: &mut Vec<f32>) -> f32 {
+    let mut power_spectrum = Vec::new();
+    let mut spectral_flux = 0.0;
 
-fn update(app: &App, model: &mut Model, _update: Update) {
-    let fft_output = model.fft_output.lock().unwrap();
-    let num_bins = fft_output.len() / 2;
+    for &value in fft_output {
+        let power = value * value;
+        power_spectrum.push(power);
+    }
 
-    let mut low_freq_sum = 0.0;
-    let mut mid_freq_sum = 0.0;
-    let mut high_freq_sum = 0.0;
-
-    let low_note_range = 0..(num_bins / 3);
-    let mid_note_range = (num_bins / 3)..(2 * num_bins / 3);
-
-    for (i, bin) in fft_output.iter().enumerate().take(num_bins) {
-        let magnitude = bin.norm();
-
-        if low_note_range.contains(&i) {
-            low_freq_sum += magnitude;
-        } else if mid_note_range.contains(&i) {
-            mid_freq_sum += magnitude;
-        } else {
-            high_freq_sum += magnitude;
+    if !prev_power_spectrum.is_empty() {
+        for i in 0..power_spectrum.len() {
+            let flux = power_spectrum[i] - prev_power_spectrum[i];
+            if flux > 0.0 {
+                spectral_flux += flux;
+            }
         }
     }
 
-    let hue_scaling_factor = 0.005;
-    let hue_change_threshold = 21.0; 
+    *prev_power_spectrum = power_spectrum;
 
-    let hue = {
-        let mut previous_hue_guard = model.previous_hue.lock().unwrap();
-        if low_freq_sum > hue_change_threshold {
-            let hue_change = low_freq_sum * hue_scaling_factor;
-            *previous_hue_guard = (*previous_hue_guard + hue_change) % 1.0;
+    spectral_flux
+}
+
+fn update(app: &App, model: &mut Model, _update: Update) {
+    let fft_output_guard = model.fft_output.lock().unwrap();
+
+    let mut fft_magnitudes: Vec<f32> = fft_output_guard.iter().map(|c| c.norm()).collect();
+
+    let spectral_flux = process_fft_output(&fft_magnitudes, &mut model.prev_power_spectrum);
+
+    let neon_hue = 0.6 + 0.3 * (model.hue / 1.0);
+
+    model.line_color = nannou::color::hsl(neon_hue, 1.0, 0.45).into();
+    model.circle_color = nannou::color::hsl(neon_hue, 1.0, 0.45).into();
+
+    let string_positions = [-90.0, -60.0, -30.0, 0.0, 30.0, 60.0];
+    model.string_points.clear();
+
+    const N: usize = 20;
+    for (index, mag) in fft_magnitudes.iter_mut().enumerate() {
+        let past_mags = &mut model.past_magnitudes[index % 6];
+        past_mags.push(*mag);
+        if past_mags.len() > N {
+            past_mags.remove(0);
         }
-        *previous_hue_guard
+        *mag = past_mags.iter().sum::<f32>() / past_mags.len() as f32;
+    }
+
+    let volume = *model.volume.lock().unwrap();
+    let amplitude = if volume > 0.0 {
+        (volume.log(10.0) * 8.5).max(1.0).min(100.0)
+    } else {
+        1.0
     };
 
-    
-    model.hue = hue;
-    model.high_freq_sum = high_freq_sum;
-    model.low_freq_sum = low_freq_sum;
+    let log_spectral_flux = (spectral_flux + 1.0).log(10.0);
+
+    let frequency_multiplier = log_spectral_flux * 0.75;
+
+    let mut target_circle_radius = 50.0;
+
+    if model.cooldown_counter > 0 {
+        model.cooldown_counter -= 1;
+    }
+
+    let spectral_flux_frames: usize = 15; // Number of past frames to average
+    model.past_spectral_flux.push(spectral_flux);
+    if model.past_spectral_flux.len() > spectral_flux_frames {
+        model.past_spectral_flux.remove(0);
+    }
+
+    let avg_spectral_flux =
+        model.past_spectral_flux.iter().sum::<f32>() / model.past_spectral_flux.len() as f32;
+
+    let beat_detection_threshold = 175.0;
+    const COOLDOWN_TIME: usize = 30;
+    if model.cooldown_counter == 0 {
+        if avg_spectral_flux > beat_detection_threshold {
+            model.hue = (model.hue + 0.3) % 1.0;
+            target_circle_radius = 100.0;
+
+            model.cooldown_counter = COOLDOWN_TIME;
+        }
+    } else {
+        model.cooldown_counter -= 1;
+    }
+
+    const DECAY_FACTOR: f32 = 0.50;
+
+    // Decay the target circle radius
+    target_circle_radius *= DECAY_FACTOR;
+
+    const SMOOTHING_FACTOR: f32 = 1.0;
+
+    model.circle_radius = model.previous_circle_radius
+        + (target_circle_radius - model.previous_circle_radius) * SMOOTHING_FACTOR;
+
+    model.previous_circle_radius = model.circle_radius;
+
+    for &position in &string_positions {
+        let mut points = Vec::new();
+        for i in 0..1000 {
+            let x = map_range(
+                i,
+                0,
+                999,
+                -app.window_rect().w() / 2.0,
+                app.window_rect().w() / 2.0,
+            );
+            let angle = map_range(i, 0, 999, 0.0, 2.0 * PI * frequency_multiplier);
+            let y = position + amplitude * angle.sin();
+            points.push(pt2(x, y));
+        }
+        model.string_points.push(points);
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
 
-    let window_rect = app.window_rect();
-    let window_width = window_rect.w();
-
-    
-    let string_positions = [-90.0, -60.0, -30.0, 0.0, 30.0, 60.0];
-
-   
-    let easing = 0.2;
-
-    for (index, &position) in string_positions.iter().enumerate() {
-        let mut points = Vec::new();
-
-        let line_color = nannou::color::hsl(model.hue, 1.0, 0.5);
-
-        let target_frequency_multiplier = model.high_freq_sum;
-
-     
-        let mut current_frequency_multiplier = model.previous_frequency_multiplier.lock().unwrap();
-        let frequency_multiplier = *current_frequency_multiplier
-            + (target_frequency_multiplier - *current_frequency_multiplier) * easing;
-        *current_frequency_multiplier = frequency_multiplier;
-
-        let volume = *model.volume.lock().unwrap();
-        let amplitude = (volume * volume) * 0.0045; 
-
-        let frequency = frequency_multiplier * 0.25; 
-
-        for i in 0..1000 {
-            let x = map_range(i, 0, 999, -window_width / 2.0, window_width / 2.0);
-            let angle = map_range(i, 0, 999, 0.0, 2.0 * PI * frequency);
-            let y = position + amplitude * angle.sin();
-            points.push(pt2(x, y));
-        }
-
-        draw.polyline().points(points).color(line_color);
+    // ocolating strings
+    for points in &model.string_points {
+        draw.polyline()
+            .points(points.clone())
+            .color(model.line_color);
     }
 
-    draw_pulsating_circle(&draw, model);
-
-    draw.to_frame(app, &frame).unwrap();
-}
-
-fn draw_pulsating_circle(draw: &Draw, model: &Model) {
-  
-    let circle_scaling_factor = 20.0; 
-
-    
-    let circle_easing = 1000.0;
-
-    let target_circle_radius = model.low_freq_sum * circle_scaling_factor;
-    let mut previous_circle_radius = model.previous_circle_radius.lock().unwrap();
-    let circle_radius =
-        *previous_circle_radius + (target_circle_radius - *previous_circle_radius) * circle_easing;
-    *previous_circle_radius = circle_radius;
-
-    let circle_color = nannou::color::hsl(model.hue, 1.0, 0.5);
-
-    
-    let circle_size = 32.5; 
-
-    
-    let min_circle_size = 30.0; 
-    let max_circle_size = 35.0; 
-    let pulsation_speed = 0.9; 
-
-    
-    let target_circle_size =
-        min_circle_size + (max_circle_size - min_circle_size) * model.high_freq_sum;
-    let pulsating_circle_size = circle_size + (target_circle_size - circle_size) * pulsation_speed;
-
+    //circle
     draw.ellipse()
         .x_y(0.0, 150.0)
-        .radius(pulsating_circle_size)
-        .color(circle_color);
+        .radius(model.circle_radius)
+        .color(model.circle_color);
+
+    draw.to_frame(app, &frame).unwrap();
 }
